d2d8098d5d731e3758aad684880a06b5ffdcb914
MEOFIXBUG <57151978+MEOFIXBUG@users.noreply.github.com>
2020-01-01 01:02:07 +0700

    update delete (db)

--- SDG/SDG/app/utils/db.js
+++ SDG/SDG/app/utils/db.js
@@ -4,7 +4,7 @@ function connectToSring() {
         host: 'localhost',
         port: '3306',
         user: 'root',
-        database: 'auction'
+        database: 'myauction'
     });
 }
 
@@ -27,3 +27,45 @@ exports.load = sql => {
         con.end();
     });
 };
+exports.del = (tbName, idField, id) => {
+    return new Promise((resolve, reject) => {
+        const con = connectToSring();
+        con.connect(err => {
+            if (err) {
+                reject(err);
+            }
+            //console.log("Connected!");
+        });
+        let sql = 'delete from ?? where ?? = ?';
+        const params = [tbName, idField, id];
+        sql = mysql.format(sql, params);
+        con.query(sql, (error, results, fields) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(results.affectedRows);
+        });
+        con.end();
+    });
+};
+exports.update = (tbName, idField, entity) => {
+    return new Promise((resolve, reject) => {
+        const con = connectToSring();
+        con.connect(err => {
+            if (err) {
+                reject(err);
+            }
+            //console.log("Connected!");
+        });
+        const id = entity[idField];
+        delete entity[idField];
+        let sql = `update ${tbName} set ? where ${idField}=${id}`;
+        con.query(sql, (error, results, fields) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(results.changedRows);
+        });
+        con.end();
+    })
+}
