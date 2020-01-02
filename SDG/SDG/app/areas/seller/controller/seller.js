e6ef09928d1e140d7cdf70ab5463cd8f7c3e622e
Tran Ngoc <hocmai6@gmail.com>
2020-01-03 00:38:19 +0700

    #router
    - Router  feature view bidder, view detail, review bidder and cancel sold

--- SDG/SDG/app/areas/seller/controller/seller.js
+++ SDG/SDG/app/areas/seller/controller/seller.js
@@ -5,6 +5,7 @@ const multipleUploadMiddleware = require("../../../middleware/multipleUploadMidd
 const auth = require('../../../utils/auth');
 const mPro = require('../../../models/product');
 const mCat = require('../../../models/category');
+const mRe = require('../../../models/review');
 var moment = require('moment');
 
 var fs = require('fs');
@@ -93,7 +94,50 @@ router.post('/multiple-upload', async(req, res) => {
         });
     }
 });
+router.post('/reviewPost', async(req, res) => {
+    try {
+        const data = JSON.parse(JSON.stringify(req.body));
+        var point = data.star;
+        var rev = data.review;
+        var bidderID = data.ID;
+        const token = req.cookies.jwt;
+        const payload = await auth.verifyToken(token);
+        var uID = payload.uID;
+        console.log(data);
+        var start = 0;
+        if (point =='+1')
+        {
+            star = 1;
+        }
+        const sql = await mRe.reviewBidder(bidderID,uID,star,`'${rev}'`);
 
+        res.render('seller/Success', {
+            body: `Review thành công`
+        });
+    } catch (error) {
+        res.render('seller/Success', {
+            body: '<b> Quang</b>' + error
+        });
+    }
+});
+router.post('/cancelSell', async(req, res) => {
+    try {
+        const data = JSON.parse(JSON.stringify(req.body));
+        var bidderID = data.ID;
+        const token = req.cookies.jwt;
+        var rev = 'Không thanh toán';
+        const payload = await auth.verifyToken(token);
+        var uID = payload.uID;
+        const sql = await mRe.reviewBidder(bidderID,uID,0,`'${rev}'`);
+        res.render('seller/Success', {
+            body: `Hủy giao dịch thành công`
+        });
+    } catch (error) {
+        res.render('seller/Success', {
+            body: '<b> Quang</b>' + error
+        });
+    }
+});
 
 router.post('/editPost', async(req, res) => {
     try {
@@ -120,13 +164,20 @@ router.post('/editPost', async(req, res) => {
 });
 router.get('/myproduct', async(req, res) => {
     var curTime = new Date().toJSON();
-    const rows = await mPro.getRemainProduct(curTime);
+    var rows = await mPro.getRemainProduct(curTime);
     rows.forEach(element => {
         element.pubDate = moment().format("HH:mm:ss DD-MM-YYYY");
         element.endDate = moment().format("HH:mm:ss DD-MM-YYYY");
     });
+    var sold = await mPro.getSoldProduct();
+    sold.forEach(element => {
+        element.pubDate = moment().format("HH:mm:ss DD-MM-YYYY");
+        element.endDate = moment().format("HH:mm:ss DD-MM-YYYY");
+    });
+    console.log(sold);
     res.render("seller/myproduct", {
-        products: rows
+        products: rows,
+        soldProducts : sold
     });
     //   return app.use("/", router);
 });
@@ -140,6 +191,34 @@ router.get('/myproduct/:id', async(req, res) => {
     res.render("seller/edit", {
         pro: detail[0],
         cat: catname[0]
+    });
+    //   return app.use("/", router);
+});
+router.get('/bidder/:id', async(req, res) => {
+    const bidderID = parseInt(req.params.id);
+    // const proID = parseInt(req.params.id);
+    // const detail = await mPro.getOnebyId(proID);
+    // detail[0].pubDate = moment().format("HH:mm:ss DD-MM-YYYY");
+    // detail[0].endDate = moment().format("HH:mm:ss DD-MM-YYYY");
+    // const catname = await mCat.getCatNamebyID(detail[0].catId);
+    // Co ID san pham
+    res.render("seller/biddereview",
+    {
+        ID: bidderID
+    });
+    //   return app.use("/", router);
+});
+router.get('/detail/:id', async(req, res) => {
+    const proID = parseInt(req.params.id);
+    // const proID = parseInt(req.params.id);
+    // const detail = await mPro.getOnebyId(proID);
+    // detail[0].pubDate = moment().format("HH:mm:ss DD-MM-YYYY");
+    // detail[0].endDate = moment().format("HH:mm:ss DD-MM-YYYY");
+    // const catname = await mCat.getCatNamebyID(detail[0].catId);
+    // Co ID san pham
+    res.render("seller/biddereview",
+    {
+        ID: bidderID
     });
     //   return app.use("/", router);
 });
